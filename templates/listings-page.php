6085e1d090f8285a77d596cbcd0d05073cc11afe
bunthut <43689017+bunthut@users.noreply.github.com>
2025-09-05 19:11:23 +0200

    Remove external listing post type and adapt listings page for ActivityPub objects

--- templates/listings-page.php
+++ templates/listings-page.php
@@ -10,7 +10,7 @@ get_header(); ?>
     <main id="main" class="site-main fed-classifieds-listings">
         <?php
         $query = new WP_Query([
-            'post_type'      => [ 'listing', 'external_listing' ],
+            'post_type'      => [ 'listing', 'ap_object' ],
             'post_status'    => 'publish',
             'posts_per_page' => -1,
         ]);
@@ -18,14 +18,25 @@ get_header(); ?>
         if ( $query->have_posts() ) :
             while ( $query->have_posts() ) :
                 $query->the_post();
-                $link = 'external_listing' === get_post_type() ? get_post_meta( get_the_ID(), '_external_url', true ) : get_permalink();
+                $data = 'ap_object' === get_post_type() ? json_decode( get_the_content(), true ) : [];
                 ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class('fed-classifieds-listing'); ?>>
                     <header class="entry-header">
                         <?php if ( 'listing' === get_post_type() ) : ?>
                             <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        <?php else : ?>
-                            <h2 class="entry-title"><a href="<?php echo esc_url( $link ); ?>" target="_blank" rel="nofollow noopener"><?php the_title(); ?></a></h2>
+                        <?php else :
+                            $link = '';
+                            if ( isset( $data['url'] ) ) {
+                                $link = $data['url'];
+                            } elseif ( isset( $data['id'] ) ) {
+                                $link = $data['id'];
+                            }
+                            ?>
+                            <?php if ( $link ) : ?>
+                                <h2 class="entry-title"><a href="<?php echo esc_url( $link ); ?>" target="_blank" rel="nofollow noopener"><?php the_title(); ?></a></h2>
+                            <?php else : ?>
+                                <h2 class="entry-title"><?php the_title(); ?></h2>
+                            <?php endif; ?>
                         <?php endif; ?>
                     </header>
                     <div class="entry-content">
@@ -37,7 +48,6 @@ get_header(); ?>
                             }
                             the_excerpt();
                         } else {
-                            $data = json_decode( get_the_content(), true );
                             if ( isset( $data['content'] ) ) {
                                 echo wp_kses_post( wpautop( $data['content'] ) );
                             } elseif ( isset( $data['summary'] ) ) {
